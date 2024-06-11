f2db15cf47a31b81ed234364a4dc9ef3b24fd4a2
Rob Leonard <robert@zinc.work>
2024-06-11 21:31:32 +0100

    Init ShadCN component lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxtjs/storybook"],
-  typescript: {
-    typeCheck: true,
-    strict: true,
-  }
+	modules: ["@nuxtjs/storybook", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+	typescript: {
+		typeCheck: true,
+		strict: true,
+	},
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: "",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./components/ui",
+	},
 })
